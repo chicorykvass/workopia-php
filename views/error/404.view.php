be95782546ef40f8e306bf82450da1b7d8bc906a
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-04-09 21:31:55 +0300

    Minor changes

--- views/error/404.view.php
+++ views/error/404.view.php
@@ -1,12 +1,11 @@
 <?= loadPartial('head') ?>
 <?= loadPartial('navbar') ?>
-<?= loadPartial('top-banner') ?>
 
 <section>
   <div class="container mx-auto p-4 mt-4">
-    <div class="text-red-600 text-center text-8xl mb-4 font-bold border border-gray-300 p-3">404</div>
-    <p class="text-center text-2xl mb-4">
-      This page does not exist
+    <div class="text-blue-700 text-center text-7xl mb-4 font-bold border border-gray-300 p-3">404</div>
+    <p class="text-slate-500 text-center font-semibold text-3xl mb-4">
+      This page doesn't exist
     </p>
   </div>
 </section>
