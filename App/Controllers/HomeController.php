6466728ebc865724c6336840f1e4f0224cd75948
Artyom Gerasimov <tourmalineschoerl@gmail.com>
2025-04-13 05:19:18 +0300

    Added DockBlocks to controller methods

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Controllers;
 
 class HomeController extends Controller {
+
+  /**
+   * Load main page
+   *
+   * @return void
+   */
   public function index() {
     $listings = $this->db->query('SELECT id, title, description, salary, tags, city, state FROM listings LIMIT 6')->fetchAll();
 
--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -3,16 +3,32 @@
 namespace App\Controllers;
 
 class ListingController extends Controller {
+
+  /**
+   * Load listings page
+   *
+   * @return void
+   */
   public function index() {
     $listings = $this->db->query('SELECT id, title, description, salary, tags, city, state FROM listings')->fetchAll();
 
     loadView('listings/index', ['listings' => $listings]);
   }
 
+  /**
+   * Create a new listing
+   *
+   * @return void
+   */
   public function create() {
     loadView('/listings/create');
   }
 
+  /**
+   * Show listing details
+   *
+   * @return void
+   */
   public function show() {
     $id = $_GET['id'] ?? null;
     $listing = null;
